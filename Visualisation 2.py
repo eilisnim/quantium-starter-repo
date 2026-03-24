4732e0ef27851b4a9224e749319fabe86f2e1222
eilisnim <sba24404@student.cct.ie>
2026-03-24 20:21:18 +0000

    Task 4 complete - added CSS

--- Visualisation 2.py
+++ Visualisation 2.py
@@ -1,21 +1,18 @@
-# import libraries
 import pandas as pd
 import numpy as np
-from dash import Dash, html, dcc, dash_table
+from dash import Dash, html, dcc, Input, Output #callback
 from datetime import datetime
 import plotly.express as px
-# print("dash version - ", dash.__version__)
 
 df1 = pd.read_csv('data/daily_sales_data_0.csv')
 df2 = pd.read_csv('data/daily_sales_data_1.csv')
 df3 = pd.read_csv('data/daily_sales_data_2.csv')
-
 # combine into 1 dataframe
 df_candy = pd.concat([df1, df2, df3], ignore_index=True)
-
+# PREPARE DATA
 # convert date to datetime
 df_candy['date'] = pd.to_datetime(df_candy['date'])
-
+# Filter for Pink Morsels only
 df_pinkmorsel = (
     df_candy[df_candy['product'] == 'pink morsel']
     .sort_values(by='date', ascending=True)
@@ -33,6 +30,10 @@ df_pinkmorsel['quantity'] = pd.to_numeric(df_pinkmorsel['quantity'], errors='coe
 # 'sales' = 'quantity' * 'price'
 df_pinkmorsel['sales'] = df_pinkmorsel['quantity'] * df_pinkmorsel['price']
 
+# force numericc, remove NaNs
+df_pinkmorsel['sales'] = pd.to_numeric(df_pinkmorsel['sales'], errors='coerce')
+df_pinkmorsel = df_pinkmorsel.dropna(subset=['sales'])
+
 # clean df for Dash
 df_output = df_pinkmorsel.drop(columns=['product', 'price', 'quantity'])
 
@@ -40,14 +41,12 @@ df_output = df_pinkmorsel.drop(columns=['product', 'price', 'quantity'])
 df_output = df_output[['sales', 'date', 'region']].copy()
 
 # output CSV file
-# C:\Users\eilis\OneDrive\Documents\Forage SD Pycharm\data
 df_output.to_csv(r"C:\Users\eilis\Downloads\Vis_pink_morsel_output.csv", index=False)
 
 np.random.seed(42)
-
 df_grouped = (
     df_output
-    .groupby([pd.Grouper(key='date', freq='MS'), 'region'])
+    .groupby([pd.Grouper(key='date', freq='3D'), 'region']) # every 3 days
     .agg(
         sales=('sales', 'sum'),
         last_date=('date', 'max')  # last actual date in the bucket, 'last_date' is new col in df
@@ -55,62 +54,134 @@ df_grouped = (
     .reset_index()
 )
 
-# REMOVE all records for first and last month, incomplete months, causing inaccurate graph, due to groupby behavour
-#Find the first and last months directly from the date column
-first_month = df_grouped['date'].dt.to_period('M').min()
-last_month = df_grouped['date'].dt.to_period('M').max()
-
-#Filter them out
-df_complete = df_grouped[
-    (df_grouped['date'].dt.to_period('M') != first_month) &
-    (df_grouped['date'].dt.to_period('M') != last_month)
+df_grouped = df_grouped.sort_values('date')
+# filter for graph date-range
+df_filtered = df_grouped[
+    (df_grouped["date"] >= "2020-12-15") &
+    (df_grouped["date"] <= "2021-02-01")
 ]
-df_complete = df_complete.sort_values('date')
+df_filtered['region'] = df_filtered['region'].str.title()
+df_complete = df_filtered
 
 # create line chart
 fig1 = px.line(
-    df_complete,
+    df_filtered,
     x="date",   # date uses month start
     y="sales",
     color="region",
     markers=True,
-    hover_data={
-        "date": "|%b %Y",
-        "sales": ":,.0f"
-    }
-)
-
-# put date in milliseconds - Price Increase Date
-dt_obj = datetime.strptime('15.01.2021 00:00:00,76',
-                           '%d.%m.%Y %H:%M:%S,%f')
-DtPriceInc_millisec = dt_obj.timestamp() * 1000
-
-print('Date of price inc. in millisecs: ', DtPriceInc_millisec)
-# vertical line showing date of price increase
-fig1.add_vline(
-    x=DtPriceInc_millisec,
-    line_dash="solid",
-    line_color="red",
-    annotation_text="Price Increase (15 Jan 2021)",
-    annotation_position="top right"
 )
 
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(
-        "Sweet Sales before and after price increase",
-        style={'textAlign': 'center'}
-    ),
-    html.H2(
-        "Pink Morsels",
-        style={'textAlign': 'center'}
-    ),
-    dcc.Graph(
-        id='sales-graph',
-        figure=fig1
-    )
+        "Pink Morsel Sales before and after price increase",
+        style={
+            "textAlign": "center",
+            "backgroundColor": "#fff0f5",
+            "padding": "50px",
+            "margin": "0",
+            "fontSize": "28px",
+            "fontWeight": "bold"
+    }),
+    html.Div([
+        # left panel
+        html.Div([
+            dcc.Graph(
+                id='sales-graph',
+                figure=fig1
+            )
+        ],
+        style={
+            'flex': '2',
+            'padding': '10px',
+        }),
+        # right panel
+        html.Div([
+            html.H3(
+                "Select Sales Region",
+                className="radio-title"
+            ),
+            dcc.RadioItems(
+                id='radio-region',
+                options=[
+                    {'label': 'North', 'value': 'north'},
+                    {'label': 'South', 'value': 'south'},
+                    {'label': 'East', 'value': 'east'},
+                    {'label': 'West', 'value': 'west'},
+                    {'label': 'All Regions', 'value': 'all'},
+                ],
+                value='all',
+                labelClassName = "radio-label"
+            ),
+        html.Img(
+            src="/assets/pink_morsel.png",
+            className="controls-img"
+        )
+    ], className="controls-panel")
+], className="main-container")
 ])
+@app.callback(
+    Output('sales-graph', 'figure'),
+    Input('radio-region', 'value')
+)
+def update_graph(selected_region):
+    # Filter data-radio buttons
+    if selected_region == "all":
+        df_filtered = df_complete
+    else:
+        df_filtered = df_complete[
+            df_complete["region"].str.lower() == selected_region
+        ]
+    # Create updated figure
+    fig1 = px.line(
+        df_filtered,
+        x="date",
+        y="sales",
+        color="region",
+        markers=True
+    )
+    fig1.update_layout(
+        plot_bgcolor='#EFC9E4',  # graph area
+        paper_bgcolor='#EFC9E4',  # border area
+        height = 400,
+        showlegend=False
+    )
+    # Hover style
+    fig1.update_traces(
+        hoverlabel=dict(
+        bgcolor='#EDAFCA',
+        font_color='black',
+        bordercolor = '#F52071' # border pink of trace
+    ),
+    hovertemplate = (
+        "<b>%{x|%d %b %Y}</b><br><br>"
+        "Region: %{fullData.name}<br>"
+        "Sales: $%{y:,.0f}"
+        "<extra></extra>"
+    )
+)
+    # Add vertical line (price increase Date)
+    dt_obj = datetime.strptime(
+        '15.01.2021 00:00:00,76',
+        '%d.%m.%Y %H:%M:%S,%f'
+    )
+    dt_obj = datetime.strptime('15.01.2021 00:00:00,76',
+                               '%d.%m.%Y %H:%M:%S,%f')
+    DtPriceInc_millisec = dt_obj.timestamp() * 1000 # put date in milliseconds
 
+    # vertical line showing date of price increase, after callback
+    fig1.add_vline(
+    #    x=dt_obj,
+        x=DtPriceInc_millisec,
+        line_dash="solid",
+        line_color="red",
+        annotation_text="Price Increase (15 Jan 2021)",
+        annotation_position="top right"
+    )
+
+    return fig1
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
+#    app.run_server(debug=True)
