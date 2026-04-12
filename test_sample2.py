73ff7487ef8b0aa4f44bc67a9f4108b5df7b4124
eilisnim <sba24404@student.cct.ie>
2026-04-12 09:45:12 +0100

    start_server(app) inside each test

--- test_sample2.py
+++ test_sample2.py
@@ -4,8 +4,7 @@ import pytest
 
 from Visualisation2 import app
 
-def test_visuals_present(dash_duo):
-
+def test_header_present(dash_duo):
     dash_duo.start_server(app)
 
     # is main header present?
@@ -17,10 +16,14 @@ def test_visuals_present(dash_duo):
     header = dash_duo.find_element("#main-header")
     assert header.text == "Pink Morsel Sales before and after price increase"
 
+def test_graph_present(dash_duo):
+    dash_duo.start_server(app)
+
     # is main graph present?
     # wait until graph exists in DOM
 
     try:
+        dash_duo.start_server(app)
         dash_duo.wait_for_element("#sales-graph", timeout=5)
     except Exception:
         assert False, "ERROR: Sales graph is not present"
@@ -31,12 +34,14 @@ def test_visuals_present(dash_duo):
     # is radio-button present?
     # wait until graph exists in DOM
 
+def test_radio_present(dash_duo):
+    dash_duo.start_server(app)
+
     try:
+        dash_duo.start_server(app)
         dash_duo.wait_for_element("#radio-region", timeout=5)
     except Exception:
         assert False, "ERROR: Radio buttons are not present"
 
     radio = dash_duo.find_element("#radio-region")
     assert radio is not None
-
-assert dash_duo.get_logs() == [], "Browser console should contain no errors"
